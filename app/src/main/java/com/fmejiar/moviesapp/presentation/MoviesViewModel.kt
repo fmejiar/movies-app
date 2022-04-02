676be075ae8acf73ee4392975bca3b36f1317e87
Frank Mejia <frankmejiarios@gmail.com>
2022-04-01 22:52:58 -0500

    #feature Adding view model factory and get upcoming movies function in movies view model.

--- app/src/main/java/com/fmejiar/moviesapp/presentation/MoviesViewModel.kt
+++ app/src/main/java/com/fmejiar/moviesapp/presentation/MoviesViewModel.kt
@@ -1,4 +1,28 @@
 package com.fmejiar.moviesapp.presentation
 
-class MoviesViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
+import com.fmejiar.moviesapp.core.UpcomingMoviesResult
+import com.fmejiar.moviesapp.domain.repository.MoviesRepository
+import kotlinx.coroutines.Dispatchers
+import java.lang.Exception
+
+class MoviesViewModel(private val moviesRepository: MoviesRepository): ViewModel() {
+
+    fun fetchUpcomingMovies() = liveData(Dispatchers.IO) {
+       emit(UpcomingMoviesResult.Loading())
+       try {
+           emit(UpcomingMoviesResult.Success(moviesRepository.getUpcomingMovies()))
+       } catch (e: Exception) {
+           emit(UpcomingMoviesResult.Failure(e))
+       }
+    }
+
+}
+
+class MoviesViewModelFactory(private val moviesRepository: MoviesRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(MoviesRepository::class.java).newInstance(moviesRepository)
+    }
 }
