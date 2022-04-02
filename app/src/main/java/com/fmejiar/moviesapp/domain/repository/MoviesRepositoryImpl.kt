0ee9dc91108576209779776b6ac2dc8111e464e3
Frank Mejia <frankmejiarios@gmail.com>
2022-04-01 21:49:32 -0500

    #feature Implementing movies repository.

--- app/src/main/java/com/fmejiar/moviesapp/domain/repository/MoviesRepositoryImpl.kt
+++ app/src/main/java/com/fmejiar/moviesapp/domain/repository/MoviesRepositoryImpl.kt
@@ -1,7 +1,10 @@
 package com.fmejiar.moviesapp.domain.repository
 
-class MoviesRepositoryImpl: MoviesRepository {
+import com.fmejiar.moviesapp.data.model.MovieList
+import com.fmejiar.moviesapp.data.remote.MoviesDataSource
 
+class MoviesRepositoryImpl(private val moviesDataSource: MoviesDataSource): MoviesRepository {
 
+    override suspend fun getUpcomingMovies(): MovieList = moviesDataSource.getUpcomingMovies()
 
 }
