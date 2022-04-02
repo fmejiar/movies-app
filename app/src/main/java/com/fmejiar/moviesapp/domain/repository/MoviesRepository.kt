c8b3075558e1280d0272d8052347cd2d028b870e
Frank Mejia <frankmejiarios@gmail.com>
2022-04-01 21:48:34 -0500

    #feature Adding get upcoming movies suspend function in movies repository.

--- app/src/main/java/com/fmejiar/moviesapp/domain/repository/MoviesRepository.kt
+++ app/src/main/java/com/fmejiar/moviesapp/domain/repository/MoviesRepository.kt
@@ -1,4 +1,9 @@
 package com.fmejiar.moviesapp.domain.repository
 
+import com.fmejiar.moviesapp.data.model.MovieList
+
 interface MoviesRepository {
+
+    suspend fun getUpcomingMovies(): MovieList
+
 }
