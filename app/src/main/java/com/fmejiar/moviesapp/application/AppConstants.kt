fecd4b71d3a3bc7b6ea1442d05977a3fae76af40
Frank Mejia <frankmejiarios@gmail.com>
2022-04-01 21:32:56 -0500

    #feature Creating singleton app constants.

---
+++ app/src/main/java/com/fmejiar/moviesapp/application/AppConstants.kt
@@ -0,0 +1,6 @@
+package com.fmejiar.moviesapp.application
+
+object AppConstants {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = "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"
+}
