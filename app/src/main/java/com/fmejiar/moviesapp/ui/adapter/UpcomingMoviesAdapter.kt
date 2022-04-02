a8eb0941de41331e9e96e3e8740441acb9809ad8
Frank Mejia <frankmejiarios@gmail.com>
2022-04-02 01:53:52 -0500

    #feature Creating upcoming movies adapter.

---
+++ app/src/main/java/com/fmejiar/moviesapp/ui/adapter/UpcomingMoviesAdapter.kt
@@ -0,0 +1,62 @@
+package com.fmejiar.moviesapp.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.fmejiar.moviesapp.core.BaseViewHolder
+import com.fmejiar.moviesapp.data.model.Movie
+import com.fmejiar.moviesapp.databinding.MovieItemBinding
+import androidx.recyclerview.widget.DiffUtil.DiffResult.NO_POSITION
+
+class UpcomingMoviesAdapter(
+    private val itemClickListener: OnUpcomingMovieClickListener
+) : RecyclerView.Adapter<BaseViewHolder<*>>() {
+
+    private var upcomingMovies = listOf<Movie>()
+
+    interface OnUpcomingMovieClickListener {
+        fun onUpcomingMovieClick(movie: Movie, position: Int)
+    }
+
+    fun setUpcomingMovies(upcomingMovies: List<Movie>) {
+        this.upcomingMovies = upcomingMovies
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
+        val itemBinding =
+            MovieItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val holder = UpcomingMovieViewHolder(itemBinding)
+
+        itemBinding.root.setOnClickListener {
+            val position = holder.adapterPosition.takeIf { it != NO_POSITION }
+                ?: return@setOnClickListener
+            itemClickListener.onUpcomingMovieClick(upcomingMovies[position], position)
+        }
+
+        return holder
+    }
+
+    override fun getItemCount(): Int = upcomingMovies.size
+
+    override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
+        when (holder) {
+            is UpcomingMovieViewHolder -> holder.bind(upcomingMovies[position])
+        }
+    }
+
+    private inner class UpcomingMovieViewHolder(
+        private val binding: MovieItemBinding
+    ) : BaseViewHolder<Movie>(binding.root) {
+
+        override fun bind(item: Movie) {
+            Glide.with(binding.upcomingMovieImageView.context)
+                .load("https://image.tmdb.org/t/p/w500/${item.poster_path}")
+                .centerCrop()
+                .into(binding.upcomingMovieImageView)
+        }
+
+    }
+
+}
