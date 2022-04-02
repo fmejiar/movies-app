d8ef174f8b709fc6bbb2ea6fbb5f7ded34233d90
Frank Mejia <frankmejiarios@gmail.com>
2022-04-01 21:45:16 -0500

    #feature Updating singleton app constants.

--- app/src/main/java/com/fmejiar/moviesapp/application/AppConstants.kt
+++ app/src/main/java/com/fmejiar/moviesapp/application/AppConstants.kt
@@ -2,5 +2,5 @@ package com.fmejiar.moviesapp.application
 
 object AppConstants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
-    const val API_KEY = "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"
+    const val API_KEY = "c9304836e5e4251ca02c513346dfe7d6"
 }
