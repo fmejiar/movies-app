162685dc830562684cf22ec83683a29a08f3640f
Frank Mejia <frankmejiarios@gmail.com>
2022-04-03 03:17:15 -0500

    #feature Adding do logIn use case and do LogIn function in movies view model.

--- app/src/main/java/com/fmejiar/moviesapp/presentation/MoviesViewModel.kt
+++ app/src/main/java/com/fmejiar/moviesapp/presentation/MoviesViewModel.kt
@@ -4,25 +4,38 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.liveData
 import com.fmejiar.moviesapp.core.UpcomingMoviesResult
+import com.fmejiar.moviesapp.domain.usecase.DoLogInUseCase
 import com.fmejiar.moviesapp.domain.usecase.GetUpcomingMoviesUseCase
 import kotlinx.coroutines.Dispatchers
 import java.lang.Exception
 
-class MoviesViewModel(private val getUpcomingMoviesUseCase: GetUpcomingMoviesUseCase): ViewModel() {
+class MoviesViewModel(
+    private val getUpcomingMoviesUseCase: GetUpcomingMoviesUseCase,
+    private val doLogInUseCase: DoLogInUseCase
+) : ViewModel() {
 
     fun fetchUpcomingMovies() = liveData(Dispatchers.IO) {
-       emit(UpcomingMoviesResult.Loading())
-       try {
-           emit(UpcomingMoviesResult.Success(getUpcomingMoviesUseCase()))
-       } catch (e: Exception) {
-           emit(UpcomingMoviesResult.Failure(e))
-       }
+        emit(UpcomingMoviesResult.Loading())
+        try {
+            emit(UpcomingMoviesResult.Success(getUpcomingMoviesUseCase()))
+        } catch (e: Exception) {
+            emit(UpcomingMoviesResult.Failure(e))
+        }
     }
+
+    suspend fun doLogin(user: String, password: String): Boolean =
+        doLogInUseCase.invoke(user, password)
 
 }
 
-class MoviesViewModelFactory(private val getUpcomingMoviesUseCase: GetUpcomingMoviesUseCase): ViewModelProvider.Factory {
+class MoviesViewModelFactory(
+    private val getUpcomingMoviesUseCase: GetUpcomingMoviesUseCase,
+    private val doLogInUseCase: DoLogInUseCase
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return modelClass.getConstructor(GetUpcomingMoviesUseCase::class.java).newInstance(getUpcomingMoviesUseCase)
+        return modelClass.getConstructor(
+            GetUpcomingMoviesUseCase::class.java,
+            DoLogInUseCase::class.java
+        ).newInstance(getUpcomingMoviesUseCase, doLogInUseCase)
     }
 }
