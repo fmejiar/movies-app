011bdb9b666cd7b6faaf7a608acc1bfa0d139e6d
Frank Mejia <frankmejiarios@gmail.com>
2022-04-02 20:19:07 -0500

    #feature Adding room movie dao.

---
+++ app/src/main/java/com/fmejiar/moviesapp/data/local/MovieDao.kt
@@ -0,0 +1,18 @@
+package com.fmejiar.moviesapp.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.fmejiar.moviesapp.data.model.MovieEntity
+
+@Dao
+interface MovieDao {
+
+    @Query("SELECT * FROM movieentity")
+    suspend fun getRoomUpcomingMovies(): List<MovieEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveUpcomingMovie(movie: MovieEntity)
+
+}
