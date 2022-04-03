38e41d2e801336b1f66fc13960f24524844284e1
Frank Mejia <frankmejiarios@gmail.com>
2022-04-02 21:41:05 -0500

    #feature Consuming upcoming movies web service and saving them in a room upcoming movies table.

--- app/src/main/java/com/fmejiar/moviesapp/domain/repository/MoviesRepositoryImpl.kt
+++ app/src/main/java/com/fmejiar/moviesapp/domain/repository/MoviesRepositoryImpl.kt
@@ -1,10 +1,20 @@
 package com.fmejiar.moviesapp.domain.repository
 
+import com.fmejiar.moviesapp.core.toMovieEntity
+import com.fmejiar.moviesapp.data.local.LocalMoviesDataSource
 import com.fmejiar.moviesapp.data.model.MovieList
 import com.fmejiar.moviesapp.data.remote.RemoteMoviesDataSource
 
-class MoviesRepositoryImpl(private val remoteMoviesDataSource: RemoteMoviesDataSource): MoviesRepository {
+class MoviesRepositoryImpl(
+    private val remoteMoviesDataSource: RemoteMoviesDataSource,
+    private val localMoviesDataSource: LocalMoviesDataSource
+) : MoviesRepository {
 
-    override suspend fun getUpcomingMovies(): MovieList = remoteMoviesDataSource.getUpcomingMovies()
+    override suspend fun getUpcomingMovies(): MovieList {
+        remoteMoviesDataSource.getUpcomingMovies().results.map { movie ->
+            localMoviesDataSource.saveUpcomingMovie(movie.toMovieEntity())
+        }
+        return localMoviesDataSource.getLocalUpcomingMovies()
+    }
 
 }
