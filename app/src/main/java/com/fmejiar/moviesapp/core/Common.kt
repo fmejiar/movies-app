c49cb18e43de7d52c2fbea7d87aa695d3edd5067
Frank Mejia <frankmejiarios@gmail.com>
2022-04-02 21:06:35 -0500

    #feature Creating extension function mappers.

--- app/src/main/java/com/fmejiar/moviesapp/core/Common.kt
+++ app/src/main/java/com/fmejiar/moviesapp/core/Common.kt
@@ -3,6 +3,9 @@ package com.fmejiar.moviesapp.core
 import android.content.Context
 import android.util.Patterns
 import android.widget.Toast
+import com.fmejiar.moviesapp.data.model.Movie
+import com.fmejiar.moviesapp.data.model.MovieEntity
+import com.fmejiar.moviesapp.data.model.MovieList
 
 fun String.isValidEmail(): Boolean =
     Patterns.EMAIL_ADDRESS.matcher(this).matches()
@@ -10,3 +13,25 @@ fun String.isValidEmail(): Boolean =
 fun Context.toast(text: String) {
     Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
 }
+
+fun List<MovieEntity>.toMovieList(): MovieList = MovieList(
+    this.map {
+        it.toMovie()
+    }
+)
+
+fun MovieEntity.toMovie(): Movie = Movie(
+    this.id,
+    this.adult,
+    this.backdrop_path,
+    this.original_title,
+    this.original_language,
+    this.overview,
+    this.popularity,
+    this.poster_path,
+    this.release_date,
+    this.title,
+    this.video,
+    this.vote_average,
+    this.vote_count
+)
