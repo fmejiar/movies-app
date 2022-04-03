bd548c7231cad252112fa11ea25d1e25a9a36e6c
Frank Mejia <frankmejiarios@gmail.com>
2022-04-02 21:39:18 -0500

    #feature Adding save upcoming movie function in local movies data source.

--- app/src/main/java/com/fmejiar/moviesapp/data/local/LocalMoviesDataSource.kt
+++ app/src/main/java/com/fmejiar/moviesapp/data/local/LocalMoviesDataSource.kt
@@ -1,6 +1,7 @@
 package com.fmejiar.moviesapp.data.local
 
 import com.fmejiar.moviesapp.core.toMovieList
+import com.fmejiar.moviesapp.data.model.MovieEntity
 import com.fmejiar.moviesapp.data.model.MovieList
 
 class LocalMoviesDataSource(private val moviesDao: MoviesDao) {
@@ -8,4 +9,6 @@ class LocalMoviesDataSource(private val moviesDao: MoviesDao) {
     suspend fun getLocalUpcomingMovies(): MovieList {
         return moviesDao.getRoomUpcomingMovies().toMovieList()
     }
+
+    suspend fun saveUpcomingMovie(movie: MovieEntity) = moviesDao.saveUpcomingMovie(movie)
 }
